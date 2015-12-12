dba81bad0f395913e1fc4252c7f0f76d457b09ef
Leif Andersen <leif@leifandersen.net>
2015-12-12 17:37:02 -0500

    Add rackunit-doc to build-deps.

--- info.rkt
+++ info.rkt
@@ -2,6 +2,6 @@
 (define collection "txexpr")
 (define deps '("base" "sugar" "rackunit-lib"))
 (define update-implies '("sugar"))
-(define build-deps '("scribble-lib" "racket-doc"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-doc"))
 (define scribblings '(("scribblings/txexpr.scrbl" ())))
 (define compile-omit-paths '("tests.rkt"))
