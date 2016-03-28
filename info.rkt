b96d0d5e5681f70145577628ad279f11362bdedb
Matthew Butterick <mb@mbtype.com>
2016-03-27 17:23:42 -0700

    adjust info.rkt

--- info.rkt
+++ info.rkt
@@ -4,5 +4,3 @@
 (define deps '("base" ["sugar" #:version "0.2"] "rackunit-lib"))
 (define update-implies '("sugar"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-doc"))
-(define scribblings '(("txexpr/scribblings/txexpr.scrbl" ())))
-(define compile-omit-paths '("txexpr/tests.rkt"))
---
+++ txexpr/info.rkt
@@ -0,0 +1,4 @@
+#lang info
+
+(define scribblings '(("scribblings/txexpr.scrbl" ())))
+(define compile-omit-paths '("tests.rkt"))
