f4db3731f5be3740c9f325bac09f204cbec9b2b7
Matthew Butterick <mb@mbtype.com>
2016-03-27 17:07:29 -0700

    reorg source

--- info.rkt
+++ info.rkt
@@ -1,8 +1,8 @@
 #lang info
 (define version "0.2")
-(define collection "txexpr")
+(define collection 'multi)
 (define deps '("base" ["sugar" #:version "0.2"] "rackunit-lib"))
 (define update-implies '("sugar"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-doc"))
-(define scribblings '(("scribblings/txexpr.scrbl" ())))
-(define compile-omit-paths '("tests.rkt"))
+(define scribblings '(("txexpr/scribblings/txexpr.scrbl" ())))
+(define compile-omit-paths '("txexpr/tests.rkt"))
